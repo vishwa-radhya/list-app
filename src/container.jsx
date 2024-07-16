23b1ee2a427945e30f0071091d6b8ab0586d3a80
vishwa-radhya <vishuvishwa735@gmail.com>
2024-07-16 22:27:03 +0530

    added loader

--- src/container.jsx
+++ src/container.jsx
@@ -1,9 +1,18 @@
 import Interactor from "./interactor";
 import catImage from './assets/cat.jpg';
+import Loader from "./loader";
+import { useState } from "react";
 const Container=()=>{
+    const [imgLoaded,setImgLoaded]=useState(true);
+
+    function imgLoadingHandler(){
+        setImgLoaded(false);
+    }
+
     return(
         <div className="container">
-            <img src={catImage} width={200} alt="shopping-cat" />
+            {imgLoaded && <Loader/>}
+            <img src={catImage} width={200} alt="shopping-cat" onLoad={imgLoadingHandler} />
             <Interactor/>
         </div>
     )
