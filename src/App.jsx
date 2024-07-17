1e8814c1e921e9981f6de8e66c849f3771c815b0
vishwa-radhya <vishuvishwa735@gmail.com>
2024-07-17 21:31:42 +0530

    fix auth errors

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import './App.css'
 import Container from './container/container.component'
 import Auth from './auth/Auth.component';
-import { onAuthStateChanged } from 'firebase/auth';
+import { getRedirectResult, onAuthStateChanged } from 'firebase/auth';
 import { auth } from './utils/firebase';
 import Loader from './loader/loader.component';
 function App() {
@@ -10,12 +10,25 @@ function App() {
     const [loading,setLoading] = useState(true);
 
     useEffect(()=>{
-      const unsubscribe = onAuthStateChanged(auth,(user)=>{
-        setUser(user);
-        setLoading(false);
-      })
-      return ()=>unsubscribe();
+      const checkAuthState = async()=>{
+        try{
+          const result = await getRedirectResult(auth);
+          if(result && result.user){
+            setUser(result.user);
+          }else{
+            onAuthStateChanged(auth,(user)=>{
+              setUser(user);
+              setLoading(false);
+            })
+          }
+        }catch(e){
+          console.error('Error handling redirect result',e);
+          setLoading(false);
+        }
+      }
+      checkAuthState();
     },[]);
+
     function handleAuth(user){
       setUser(user);
     }
