33226c4bcc4d89e95b81b52542a2ea9ae271fc24
vishwa-radhya <vishuvishwa735@gmail.com>
2024-07-19 10:45:10 +0530

    refactor redundant code

--- src/auth/Auth.component.jsx
+++ src/auth/Auth.component.jsx
@@ -1,23 +1,8 @@
 import TypeWriter from '../typewriter/TypeWriter.component';
 import './Auth.styles.css';
-import {auth,signInWithGoogle} from '../utils/firebase.js';
-import {  onAuthStateChanged } from 'firebase/auth';
-import { useContext, useEffect } from 'react';
-import { AuthContext } from '../contexts/authContext.jsx';
-const Auth=()=>{
-  const {handleSetUser}=useContext(AuthContext);
-    useEffect(()=>{
-          const unsubscribe = onAuthStateChanged(auth, (user) => {
-            if (user) {
-              handleSetUser(user);
-            }else{
-              console.log('no user found');
-            }
-          });
-      
-          return () => unsubscribe();
-    },[handleSetUser]);
+import {signInWithGoogle} from '../utils/firebase.js';
 
+const Auth=()=>{
     return(
         <div className="auth-container">
         <img src="https://v-list-app.netlify.app/assets/auth-icon.png" alt="" />
