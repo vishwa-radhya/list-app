7431a68754db463fca143fc3ba3c7088139404d2
vishwa-radhya <vishuvishwa735@gmail.com>
2024-07-20 16:05:02 +0530

    new-feature

---
+++ src/dropdown/Dropdown.component.jsx
@@ -0,0 +1,43 @@
+import './Dropdown.styles.css'
+import PropTypes from 'prop-types';
+import { signOutUser } from '../utils/firebase';
+import { signInWithGoogle } from '../utils/firebase';
+const Dropdown=({userImg,displayName,userEmail,isDropdownOpen})=>{
+
+    const userEmailStyles={
+        fontSize:userEmail.length<=19 ? '0.7rem' : '0.55rem',
+    }
+    async function handleSignOutUser(){
+        await signOutUser();
+    }
+    const dropdownStyles={
+        width:isDropdownOpen ? '60%' : '0%',
+        height:isDropdownOpen ? '50%' : '0%'
+    }
+    return(
+        <div className="drop-down-container" style={dropdownStyles}>
+            <div className="dd-block-1">
+            <div className="profile-pic">
+                <img src={userImg} alt="" />
+            </div>
+            </div>
+            <div className="dd-block-2">
+                <h4>{displayName}</h4>
+                <p style={userEmailStyles}>{userEmail}</p>
+                <div className="switch-acc" onClick={signInWithGoogle}>
+                <i className="fa-solid fa-users"></i><span>Switch Account</span>
+                </div>
+                <div className="sign-out-user" onClick={handleSignOutUser}>
+                <i className="fa-solid fa-arrow-right-from-bracket"></i><span>Sign Out</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+Dropdown.propTypes={
+    userImg:PropTypes.string,
+    displayName:PropTypes.string,
+    userEmail:PropTypes.string,
+    isDropdownOpen:PropTypes.bool,
+}
+export default Dropdown;
