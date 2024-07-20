7df19d26987a45820433f9ebf880cfce952c075f
vishwa-radhya <vishuvishwa735@gmail.com>
2024-07-20 16:41:20 +0530

    adjust user block

--- src/container/container.component.jsx
+++ src/container/container.component.jsx
@@ -2,14 +2,34 @@ import Interactor from "../interactor/interactor.component";
 import catImage from '../assets/cat.jpg';
 import Loader from "../loader/loader.component";
 import Dropdown from "../dropdown/Dropdown.component";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { auth } from "../utils/firebase";
 const Container=()=>{
     const [imgLoaded,setImgLoaded]=useState(true);
     const [isDropdownOpen,setIsDropdownOpen]=useState(false);
+    const dropdownRef = useRef(null);
+    const userImgRef = useRef(null);
     const userImg = auth.currentUser.photoURL;
     const displayName = auth.currentUser.displayName;
     const userEmail = auth.currentUser.email;
+
+    function handleClickOutside(event){
+        if(dropdownRef.current && !dropdownRef.current.contains(event.target) && !userImgRef.current.contains(event.target)){
+            setIsDropdownOpen(false);
+        }
+    }
+
+    useEffect(()=>{
+        if(isDropdownOpen){
+            document.addEventListener('click',handleClickOutside);
+        }else{
+            document.removeEventListener('click',handleClickOutside);
+        }
+        return ()=>{
+            document.removeEventListener('click',handleClickOutside);
+        }
+    },[isDropdownOpen]);
+
     function imgLoadingHandler(){
         setImgLoaded(false);
     }
@@ -20,12 +40,14 @@ const Container=()=>{
     return(
         <div className="container">
         <div className="s-out-pic-container">
-        <div onClick={dropdrophandler}>{userImg && <img src={userImg} alt="" width={25}></img> }</div>
+        <div onClick={dropdrophandler} ref={userImgRef}>{userImg && <img src={userImg} alt="" width={25}></img> }</div>
         </div>
             
                 <img src={catImage} width={200} alt="shopping-cat" onLoad={imgLoadingHandler} />
             {imgLoaded ? <Loader/> : <Interactor/>}
+            <div ref={dropdownRef}>
             <Dropdown userImg={userImg} displayName={displayName} userEmail={userEmail} isDropdownOpen={isDropdownOpen} />
+            </div>
         </div>
     )
 }
