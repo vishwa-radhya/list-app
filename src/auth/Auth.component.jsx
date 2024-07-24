2215c3cebf66d0dba5865c14dfaca5c79c87feb0
vishwa-radhya <vishuvishwa735@gmail.com>
2024-07-24 14:24:56 +0530

    update auth cart image with local import

--- src/auth/Auth.component.jsx
+++ src/auth/Auth.component.jsx
@@ -1,11 +1,11 @@
 import TypeWriter from '../typewriter/TypeWriter.component';
 import './Auth.styles.css';
 import {signInWithGoogle} from '../utils/firebase.js';
-
+import cartImage from '../../favicons/favicon-32x32.png'
 const Auth=()=>{
     return(
         <div className="auth-container">
-        <img src="https://v-list-app.netlify.app/assets/auth-icon.png" alt="" />
+        <img src={cartImage} alt="" />
         <h2><i className="fa-brands fa-keycdn"></i>Authentication</h2>
         <TypeWriter text='  Sign In To Organize Your Lists...' speed={30}/>
             <button className="ggl-pop-up-btn" onClick={signInWithGoogle}>
