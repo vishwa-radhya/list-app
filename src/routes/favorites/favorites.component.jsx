a5597c749b65e52e7cf175a379771927cfaaed58
vishwa-radhya <vishuvishwa735@gmail.com>
2024-07-30 10:50:51 +0530

    applied navigation with fav and home

--- src/routes/favorites/favorites.component.jsx
+++ src/routes/favorites/favorites.component.jsx
@@ -1,12 +1,12 @@
 import Picture from "../../picture/picture.component";
-import nFavCat from '../../assets/n-fav-cat.png';
+import favCat from '../../assets/fav-cat.jpg';
 import './favorites.styles.css';
 import InputAndBtn from "../../input-and-btn/Input-and-btn.component";
 import ShoppingList from '../../shopping-list/shopping-list.component';
 const Favorites=()=>{
     return(
         <div className="fav-div-container">
-            <Picture catImage={nFavCat} pictureWidth={150} />
+            <Picture catImage={favCat} pictureWidth={150} />
             <InputAndBtn placeHolder='Enter Favourites' buttonText='Add To Favourites' pushAsFav={true}  />
             <ShoppingList isFavItemsOnly={true} />
         </div>
