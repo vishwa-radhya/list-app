5ff5caaaa4be8c03e66cda8a9aaff92979925460
vishwa-radhya <vishuvishwa735@gmail.com>
2024-08-20 08:47:32 +0530

    added about page

--- src/routes/about/about.component.jsx
+++ src/routes/about/about.component.jsx
@@ -2,6 +2,8 @@ import './about.styles.css';
 import ReactSvg from '../../assets/react-svg.svg';
 import ViteSvg from '../../assets/vite-svg.svg';
 import FirebaseSvg from '../../assets/firebase-svg.svg';
+import FontAwesomeSvg from '../../assets/font-awesome-svgr.svg';
+import NetlifySvg from '../../assets/netlify-svg.svg';
 
 const About=()=>{
     return (
@@ -23,7 +25,14 @@ const About=()=>{
                     <a href="https://vitejs.dev/" target='_blank'><img src={ViteSvg} alt="vite" /></a>
                 </div>
                 <div>
+                    <a href="https://www.netlify.com/" target='_blank'><img src={NetlifySvg} alt="netlify" /></a>
+                </div>
+            </div>
+            <h5>Utilities</h5>
+            <div className='powered-by'>
+                <div>
                     <a href="https://firebase.google.com/" target='_blank'><img src={FirebaseSvg} alt="firebase" /></a>
+                    <a href="https://fontawesome.com/" target='_blank'><img src={FontAwesomeSvg} alt="font-awesome" /></a>
                 </div>
             </div>
         </div>
